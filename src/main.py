5ccd73e8dd8777c4567f991f9cde2e1323f12552
gamefreak431 <mcleish.john@gmail.com>
2026-08-17 21:10:09 +1000

    feat(main): created main.py as starting point for program. Currently contains hardcoded prints of the textnode object. TODO: add core functionality to call and run

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("hello world")
+from textnode import TextNode, textType
+
+
+def main():
+    # Example usage of TextNode
+    text_node1 = TextNode("Hello, World!", textType.PLAIN)
+    text_node2 = TextNode("This is bold text.", textType.BOLD)
+    text_node3 = TextNode("Visit OpenAI", textType.LINK, url="https://www.boot.dev")
+
+    print(text_node1)
+    print(text_node2)
+    print(text_node3)
+
+if __name__ == "__main__":
+    main()
