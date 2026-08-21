43c7bcaa9d9921a5c9f987d350b28f30d4d99565
gamefreak431 <mcleish.john@gmail.com>
2026-08-21 22:07:51 +1000

    fix(test_htmlnode) fixed a bug caused by refactor changing attributes to props and tests failing

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,31 +3,31 @@ from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        node = HTMLNode("div", attributes={"class": "container"})
-        node2 = HTMLNode("div", attributes={"class": "container"})
+        node = HTMLNode("div", props={"class": "container"})
+        node2 = HTMLNode("div", props={"class": "container"})
         self.assertEqual(node, node2)
 
     def test_not_eq(self):
-        node = HTMLNode("div", attributes={"class": "container"})
-        node2 = HTMLNode("span", attributes={"class": "container"})
+        node = HTMLNode("div", props={"class": "container"})
+        node2 = HTMLNode("span", props={"class": "container"})
         self.assertNotEqual(node, node2)
 
     def test_not_eq_different_attributes(self):
-        node = HTMLNode("div", attributes={"class": "container"})
-        node2 = HTMLNode("div", attributes={"id": "main"})
+        node = HTMLNode("div", props={"class": "container"})
+        node2 = HTMLNode("div", props={"id": "main"})
         self.assertNotEqual(node, node2)
 
     def test_eq_non_htmlnode(self):
-        node = HTMLNode("div", attributes={"class": "container"})
+        node = HTMLNode(tag="div", props={"class": "container"})
         self.assertNotEqual(node, "This is not an HTMLNode")
 
     def test_props_to_html(self):
-        node = HTMLNode("div", attributes={"class": "container", "id": "main"})
+        node = HTMLNode(tag="div", props={"class": "container", "id": "main"})
         self.assertEqual(node.props_to_html(), ' class="container" id="main"')
 
     def test_repr(self):
-        node = HTMLNode("div", attributes={"class": "container"})
-        self.assertEqual(repr(node), "HTMLNode(tag=div, value=None, children=[], attributes={'class': 'container'})")
+        node = HTMLNode(tag="div", props={"class": "container"})
+        self.assertEqual(repr(node), "HTMLNode(tag=div, value=None, children=[], props={'class': 'container'})")
 
 if __name__ == "__main__":
     unittest.main()
