56608308cc80ffae39ef487ebb68c66363542e54
gamefreak431 <mcleish.john@gmail.com>
2026-08-21 21:44:21 +1000

    feat(LeafNode): add LeafNode torender child elements as HTML

---
+++ src/leafnode.py
@@ -0,0 +1,15 @@
+from htmlnode import HtmlNode
+
+class LeafNode(HtmlNode):
+    def __init__(self, tag: str, value: str, attributes: dict[str, str]|None=None) -> None:
+        super().__init__(tag, value, attributes=attributes)
+
+    def to_html(self) -> str:
+        if not self.value:
+            raise ValueError("LeafNode must have a value to render.")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode(tag={self.tag}, value={self.value}, attributes={self.attributes})"
